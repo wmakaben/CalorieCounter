a7dfa1f3c88b452e9be2b9e655759c7060cbf3df
William Makabenta <wmakaben@gmail.com>
2015-11-08 00:24:45 -0500

    Set up fragments

---
+++ app/src/main/java/com/example/wmakaben/caloriecounter/CustomPagerEnum.java
@@ -0,0 +1,24 @@
+package com.example.wmakaben.caloriecounter;
+
+public enum CustomPagerEnum {
+
+    RED(R.string.camera_fragment_title, R.layout.fragment_camera);//,
+    //BLUE(R.string.history_fragment_title, R.layout.view_blue);
+
+    private int mTitleResId;
+    private int mLayoutResId;
+
+    CustomPagerEnum(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+
+}
---
+++ app/src/main/java/com/example/wmakaben/caloriecounter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wmakaben.caloriecounter;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+    }
+}
